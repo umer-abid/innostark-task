8d3d17d21271339a945f5fc31fc2b2a7adc9c6d3
u-abid <umer.abid@salsoft.net>
2022-07-21 01:29:35 +0500

    actions and reducers are developed for products

--- src/components/Products/Store/index.js
+++ src/components/Products/Store/index.js
@@ -1,18 +1,38 @@
-// @Import Redux
+// @import Redux
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// @dummy data
+import { products } from "../../../Data";
 
-export const getAccounts = createAsyncThunk(
+export const getProducts = createAsyncThunk(
   "/products/getProducts",
   async (params, { dispatch, getState }) => {
     dispatch(loadingStatus(true));
-    let productsList = getState().products?.products;
-    if (productsList.length) {
-      dispatch(loadingStatus(true));
-      return productsList;
-    } else {
-      dispatch(loadingStatus(false));
-      return [];
-    }
+    dispatch(allProducts(products));
+    dispatch(loadingStatus(false));
+  }
+);
+export const addProducts = createAsyncThunk(
+  "/products/addProducts",
+  async (newProd, { dispatch, getState }) => {
+    dispatch(loadingStatus(true));
+    let allProd = getState().products.products;
+    let id = Math.floor(Math.random() * 100);
+    let data = { id: id, ...newProd };
+    let updateProduct = [data, ...allProd];
+    dispatch(allProducts(updateProduct));
+    dispatch(loadingStatus(false));
+  }
+);
+export const updateProducts = createAsyncThunk(
+  "/products/updateProducts",
+  async (updatedProd, { dispatch, getState }) => {
+    dispatch(loadingStatus(true));
+    let allProd = getState().products.products;
+    const updatedItems = allProd.map(el =>
+      el.id === updatedProd.id ? updatedProd : el
+    );
+    dispatch(allProducts(updatedItems));
+    dispatch(loadingStatus(false));
   }
 );
 
@@ -23,12 +43,15 @@ export const productsSlice = createSlice({
     products: []
   },
   reducers: {
+    allProducts: (state, action) => {
+      state.products = action.payload;
+    },
     loadingStatus: (state, action) => {
       state.loadingStatus = action.payload;
     }
   }
 });
 
-export const { loadingStatus } = productsSlice.actions;
+export const { loadingStatus, allProducts } = productsSlice.actions;
 
 export default productsSlice.reducer;
