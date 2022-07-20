f005142278515549988c236573fb419ac8c4e4eb
u-abid <umer.abid@salsoft.net>
2022-07-21 01:29:51 +0500

    A dummy data file is added

---
+++ src/Data/index.js
@@ -0,0 +1,37 @@
+export const products = [
+  {
+    id: 1,
+    name: "Mobile",
+    description: "iphone 13 pro max",
+    price: 549,
+    date: "2022-5-21"
+  },
+  {
+    id: 2,
+    name: "Tablet",
+    description: "Samsung alphet 2",
+    price: 200,
+    date: "2022-4-25"
+  },
+  {
+    id: 3,
+    name: "Macbook",
+    description: "Macbook 2020 M1 chip",
+    price: 1200,
+    date: "2022-6-22"
+  },
+  {
+    id: 4,
+    name: "Laptop",
+    description: "Dell Inspiron 5570",
+    price: 800,
+    date: "2022-2-03"
+  },
+  {
+    id: 5,
+    name: "Logitech Headphones",
+    description: "Logitech zone Wireless headphones",
+    price: 78,
+    date: "2022-3-03"
+  }
+];
